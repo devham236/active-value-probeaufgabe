1a866bf055adfb067a79f16f10080d648ad49b4a
Hamza Mirza <hamza665@hotmail.de>
2025-02-01 11:48:54 +0100

    main components created

---
+++ src/components/HeroElement.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeroElement = () => {
+  return <div></div>;
+};
+
+export default HeroElement;
---
+++ src/components/IntroBanner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IntroBanner = () => {
+  return <div></div>;
+};
+
+export default IntroBanner;
