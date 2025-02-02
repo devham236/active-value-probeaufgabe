c4e40904de5fa8af7d41563eead815f48390fbe0
Hamza Mirza <hamza665@hotmail.de>
2025-02-02 11:30:01 +0100

    intro banner styles for now

--- src/components/IntroBanner.jsx
+++ src/components/IntroBanner.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import shopwareBanner from "../assets/images/shopware_banner.png";
 
 const IntroBanner = () => {
-  return <div>IntroBanner</div>;
+  return (
+    <section className="intro-banner">
+      <div className="intro-banner-container">
+        <img src={shopwareBanner} alt="shopware banner" />
+      </div>
+    </section>
+  );
 };
 
 export default IntroBanner;
