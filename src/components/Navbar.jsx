16cacc6aaa2a49d4df126165f9b8271c8f15ecca
Hamza Mirza <hamza665@hotmail.de>
2025-02-02 11:03:33 +0100

    navabr styles and functionality finished for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../assets/logos/logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [sidebarOpened, setSidebarOpened] = useState(false);
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="company-logo">
+          <img src={logo} alt="company logo" />
+        </div>
+        <div className="menu-icon" onClick={() => setSidebarOpened(true)}>
+          <span className="material-symbols-rounded">menu</span>
+        </div>
+
+        {sidebarOpened && (
+          <div className="menu-sidebar">
+            <ul className="menu-items">
+              <li>Startseite</li>
+              <li>Kontakt</li>
+              <li>Impressum</li>
+            </ul>
+            <div className="menu-icon" onClick={() => setSidebarOpened(false)}>
+              <span className="material-symbols-rounded">close</span>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
