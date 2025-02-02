3d8a61dd3f2062934e691ed80ca0d397fced2549
Hamza Mirza <hamza665@hotmail.de>
2025-02-02 12:15:49 +0100

    hero element styles for now

--- src/components/HeroElement.jsx
+++ src/components/HeroElement.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import linkedIn from "../assets/icons/linkedin.png";
+import { socialIconsArr } from "../utils/socialIcons";
 
 const HeroElement = () => {
-  return <div>Heroelement</div>;
+  return (
+    <section className="hero-section">
+      <div className="hero-container">
+        <p className="date">April 2019</p>
+        <h2 className="title">Shopware als starker Partner</h2>
+        <div className="info-text">
+          <p>
+            Wir haben eine Vielzahl von individuellen Webshops für ein diverses
+            Kundenfeld entwickelt und umgesetzt - darunter sind namhafte
+            Unternehmen wie van Laack und die Rheinische Post.
+          </p>
+          <p>
+            Bei der Umsetzung können wir auf Shopware als zuverlässigen Partner
+            zurückgreifen. Durch die Business-Kooperation mit Shopware sind
+            unsere zertifizierten Entwickler in der Lage, unseren Kunden immer
+            die neusten, innovativsten und sichersten Shop-Lösungen anbieten zu
+            können. Um immer auf dem aktuellsten Stand zu sein, treffen wir
+            unseren Kooperationspartner auf der jährlichen Fachmesse „Shopware
+            Community Day" und stehen in einem regelmäßigen Austausch. Dadurch
+            sind wir in der Lage kurzfristig auf die neustens Entwicklungen zu
+            reagieren. Dazu gehört unter anderem die Verwendung von Shopware 6 -
+            wodurch wir schon jetzt qualitativ hochwertige
+            Headless-Shop-Projekte (API-first) umsetzen können
+          </p>
+        </div>
+        <div className="social-icons">
+          {socialIconsArr.map((icon) => (
+            <span key={icon.id}>
+              <img src={icon.src} alt="" />
+            </span>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroElement;
---
+++ src/utils/socialIcons.js
@@ -0,0 +1,28 @@
+import webIcon from "../assets/icons/web.png";
+import instagramIcon from "../assets/icons/instagram.png";
+import facebookIcon from "../assets/icons/facebook.png";
+import twitterIcon from "../assets/icons/twitter.png";
+import linkedinIcon from "../assets/icons/linkedin.png";
+
+export const socialIconsArr = [
+  {
+    id: 1,
+    src: webIcon,
+  },
+  {
+    id: 2,
+    src: instagramIcon,
+  },
+  {
+    id: 3,
+    src: facebookIcon,
+  },
+  {
+    id: 4,
+    src: twitterIcon,
+  },
+  {
+    id: 5,
+    src: linkedinIcon,
+  },
+];
