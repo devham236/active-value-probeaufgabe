f977904881db86c5ac41483282a539ad5be4d8e5
Hamza Mirza <hamza665@hotmail.de>
2025-02-04 11:24:11 +0100

    footer styles and structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import instagram from "../assets/icons/instagram.png";
+import facebook from "../assets/icons/facebook.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="company-info">
+          <div>
+            <p>Benzenbergstraße 39 - 47</p>
+            <p>40219 Düsseldorf</p>
+          </div>
+          <div>
+            <p>Telefon: +49 211/749 505 0</p>
+            <p>Email: info@active-value.de</p>
+          </div>
+        </div>
+        <div className="mobile-social-icons">
+          <span className="instagram">
+            <img src={instagram} alt="" />
+          </span>
+          <span className="facebook">
+            <img src={facebook} alt="" />
+          </span>
+        </div>
+        <div className="impressum-agb">
+          <div>
+            <p>Impressum</p>
+            <p>Datenschutz</p>
+            <p>AGB</p>
+          </div>
+          <p>Copyright © 2018 active value GmbH - Alle Rechte vorbehalten.</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
