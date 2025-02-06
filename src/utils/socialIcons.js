a0aa169e5c492ac7388366da041e1ba3f8abe7b0
Hamza Mirza <hamza665@hotmail.de>
2025-02-06 12:28:45 +0100

    clean up

--- src/utils/socialIcons.js
+++ src/utils/socialIcons.js
@@ -1,9 +1,3 @@
-// import webIcon from "../assets/icons/web.png";
-// import instagramIcon from "../assets/icons/instagram.png";
-// import facebookIcon from "../assets/icons/facebook.png";
-// import twitterIcon from "../assets/icons/twitter.png";
-// import linkedinIcon from "../assets/icons/linkedin.png";
-
 export const socialIconsArr = [
   {
     id: 1,
