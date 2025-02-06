a24b762f0d0526a593b92c60385275cb2bbb4e7a
Hamza Mirza <hamza665@hotmail.de>
2025-02-06 11:46:38 +0100

    custom reusable styles for info text

--- src/components/NewsletterBanner.jsx
+++ src/components/NewsletterBanner.jsx
@@ -5,7 +5,7 @@ const NewsletterBanner = () => {
     <section className="section newsletter-banner-section">
       <p className="info-above-title nl-above-title">Newsletter</p>
       <h2 className="title nl-title">Nichts verpassen!</h2>
-      <p className="info-text">
+      <p className="info-text nl-info-text">
         At vero eos et accusam et justo duo dolores et ea rebum.
       </p>
       <button>Newsletter abonieren</button>
